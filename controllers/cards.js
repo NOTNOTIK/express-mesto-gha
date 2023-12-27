716efdbab0ce6278e0b2f1cb26461dd2dcecd038
Egor Maltsev <egormaltsev2412@gmail.com>
2023-12-28 01:58:47 +0300

    s

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,130 @@
 const Card = require("../models/card");
 
+// Все карточки
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((card) => {
+      res.status(200).send({
+        data: card,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Произошла ошибка при получении списка карточек. Подробнее: ${err.message}`,
+      });
+    });
+};
+
+// Добавляем карточку
+module.exports.createCard = (req, res) => {
+  const { name, link, owner = req.user._id } = req.body;
+  Card.create({ name, link, owner })
+    .then((card) => {
+      res.status(201).send({
+        data: card,
+      });
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({
+          message: "Невалидные данные при создании карточки",
+        });
+      } else {
+        res.status(500).send({
+          message: `Произошла ошибка. Подробнее: ${err.message}`,
+        });
+      }
+    });
+};
+
+// Удаляем карточку
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndDelete({ _id: req.params.cardId })
+    .orFail()
+    .then((card) => {
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(400).send({
+          message: "Некорректный ID для удаления карточки",
+        });
+      } else if (err.name === "DocumentNotFoundError") {
+        res.status(404).send({
+          message: "Карточка с таким ID не найдена",
+        });
+      } else {
+        res.status(500).send({
+          message: `Произошла ошибка. Подробнее: ${err.message}`,
+        });
+      }
+    });
+};
+
+// Ставим лайк
+module.exports.likeCard = (req, res) => {
+  const userId = req.user._id;
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: userId } },
+    { new: true }
+  )
+    .orFail()
+    .then((card) => {
+      res.status(200).send({
+        data: card,
+      });
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(400).send({
+          message: "Передан некорректный ID карточки",
+        });
+      } else if (err.name === "DocumentNotFoundError") {
+        res.status(404).send({
+          message: "Карточка с таким ID не найдена",
+        });
+      } else {
+        res.status(500).send({
+          message: `Произошла ошибка. Подробнее: ${err.message}`,
+        });
+      }
+    });
+};
+
+// Убираем лайк
+module.exports.dislikeCard = (req, res) => {
+  const userId = req.user._id;
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: userId } },
+    { new: true }
+  )
+    .orFail()
+    .then((card) => {
+      res.status(200).send({
+        data: card,
+      });
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(400).send({
+          message: "Передан некорректный ID карточки",
+        });
+      } else if (err.name === "DocumentNotFoundError") {
+        res.status(404).send({
+          message: "Карточка с таким ID не найдена",
+        });
+      } else {
+        res.status(500).send({
+          message: `Произошла ошибка. Подробнее: ${err.message}`,
+        });
+      }
+    });
+};
+
+/*const Card = require("../models/card");
+
 module.exports.getCards = async (req, res) => {
   try {
     const cards = await Card.find({});
@@ -95,3 +220,4 @@ module.exports.dislikeCard = async (req, res) => {
     }
   }
 };
+*/
